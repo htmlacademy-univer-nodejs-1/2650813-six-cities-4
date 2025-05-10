a5d64e688d9e7ae59bf2b389e4030291cb356dfb
Nikita Unisikhin <nik.unisikhin.04@gmail.com>
2025-05-10 20:20:43 +0500

    done

---
+++ src/core/cli-command/import.command.ts
@@ -0,0 +1,20 @@
+import {CliCommandInterface} from './cli-command.interface.js';
+import TsvFileReader from '../file-reader/tsv-file-reader.js';
+import chalk from 'chalk';
+
+export default class ImportCommand implements CliCommandInterface {
+  public readonly name = '--import';
+
+  public execute(filename: string): void {
+    const fileReader = new TsvFileReader(filename.trim());
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+      console.log(`Не удалось импортировать данные из файла: '${chalk.red(err.message)}'`);
+    }
+  }
+}
